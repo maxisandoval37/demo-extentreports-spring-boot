a376649e1d38142f6ca96c05cba9d1f56bc0daed
Maxi Sandoval <maxisandoval98@gmail.com>
2025-03-04 20:06:58 -0300

    first commit

---
+++ src/test/java/ar/dev/maxisandoval/demoextentreportsspringboot/DemoExtentreportsSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.dev.maxisandoval.demoextentreportsspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoExtentreportsSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
